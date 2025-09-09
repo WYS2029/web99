8c4dd5e960318c05ea9f87b5ffa7bfcf94d94771
WYS2029 <61704873+WYS2029@users.noreply.github.com>
2025-09-09 16:18:50 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web99">
       <App />
       <Toaster />
     </BrowserRouter>
